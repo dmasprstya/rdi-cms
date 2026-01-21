a0a1151731f3c30dabebffe0bd0e9e4c86a35096
dmasprstya <mdimasprasetyawongo@gmail.com>
2026-01-21 10:56:06 +0700

    fix: Next.js image remote pattern for Vercel Blob

--- next.config.mjs
+++ next.config.mjs
@@ -21,7 +21,7 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
-                hostname: '**.public.blob.vercel-storage.com',
+                hostname: '*.public.blob.vercel-storage.com',
             },
         ],
         formats: ['image/avif', 'image/webp'],
